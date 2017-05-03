e8f68f395e1e3d55b45233d1657e6a98d3dc9418
Elvar <elvar16@ru.is>
2017-05-03 12:15:01 +0000

    inserta template

---
+++ Cloud++/Cloud++/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cloud__.Startup))]
+namespace Cloud__
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
