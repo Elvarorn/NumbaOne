1b388b9699765a777f8101bd85514e4e1d45eacf
Elvar <elvar16@ru.is>
2017-05-04 13:47:41 +0000

    laga forsíðu, enabla migration, breyta password requiremnets ofl, ný logo mynd ofl

--- Cloud++/Cloud++/App_Start/IdentityConfig.cs
+++ Cloud++/Cloud++/App_Start/IdentityConfig.cs
@@ -54,7 +54,7 @@ namespace Cloud__
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
+                RequireNonLetterOrDigit = false,
                 RequireDigit = true,
                 RequireLowercase = true,
                 RequireUppercase = true,
