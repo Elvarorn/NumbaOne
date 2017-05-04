a6448d48b4cc7aec124254813f5d46c3e027beb4
robertbernhardgislason <robertg15@ru.is>
2017-05-04 16:02:48 +0000

    stuff

---
+++ Cloud++/Cloud++/Models/Entities/Project.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cloud__.Models.Entities
+{
+    public class Project
+    {
+    }
+}
---
+++ Cloud++/Cloud++/Services/ProjectsService.cs
@@ -0,0 +1,13 @@
+﻿using Cloud__.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cloud__.Services
+{
+    public class ProjectsService
+    {
+        private ApplicationDbContext _db;
+    }
+}
