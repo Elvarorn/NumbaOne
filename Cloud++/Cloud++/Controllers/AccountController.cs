aa0a3adbb616ccc75fc7277d990b476cefb378e0
Dagur <dagura16@ru.com>
2017-05-05 15:29:28 +0000

    sma breytingar

--- Cloud++/Cloud++/Controllers/AccountController.cs
+++ Cloud++/Cloud++/Controllers/AccountController.cs
@@ -392,7 +392,7 @@ namespace Cloud__.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
