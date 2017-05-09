546870d3e2f39c3710c1e2d09061fe1caea7809c
Dagur <dagura16@ru.com>
2017-05-09 17:22:54 +0000

    baeta vid projectum i db

--- Cloud++/Cloud++/Controllers/HomeController.cs
+++ Cloud++/Cloud++/Controllers/HomeController.cs
@@ -6,14 +6,29 @@ using System.Web.Mvc;
 
 
 using Cloud__.Models;
+using Cloud__.Models.ViewModels;
+using System.Threading.Tasks;
+using Cloud__.Models.Entities;
+using Cloud__.Services;
 
 namespace Cloud__.Controllers
 {
     public class HomeController : Controller
     {
+        private ProjectsService _ps = new ProjectsService();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateProjectViewModel model)
+        {
+
+            _ps.CreateProject(model);
+
+            return View(model);
         }
 
         public ActionResult About()
---
+++ Cloud++/Cloud++/Models/ViewModels/CreateProjectViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Cloud__.Models.ViewModels
+{
+    public class CreateProjectViewModel
+    {
+        [Required]
+        [Display(Name = "Project name")]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+        public int projectID { get; set; }
+    }
+}
