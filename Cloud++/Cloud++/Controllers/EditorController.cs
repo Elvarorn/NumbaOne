e00271d6c3296414c29308f1e8e7399cfa9605a1
Johann Einar Isaksson <johanni16@ru.is>
2017-05-09 19:49:57 +0000

    Ace editor save button

---
+++ Cloud++/Cloud++/Controllers/EditorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Cloud__.Models;
+using Cloud__.Services;
+
+namespace Cloud__.Controllers
+{
+    public class EditorController : Controller
+    {
+        // GET: Editor
+        private FileService _fs;
+
+        public EditorController()
+        {
+            _fs = new FileService();
+        }
+
+        public ActionResult AceEditor()
+        {
+            ViewBag.Code = "alert('Hello World');";
+            ViewBag.DocumentID = 17; //Þurfum að breyta í ID-ið í gagnagrunninum, þetta er harðkóðað
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveCode(EditorViewModel model)
+        {
+            string data = model.Content;
+            System.Diagnostics.Debug.WriteLine("----SAVE data------");
+            System.Diagnostics.Debug.WriteLine(model.Content);
+            System.Diagnostics.Debug.WriteLine(data);
+
+            _fs.SaveData(data);
+
+            return View(RedirectToAction("AceEditor"));
+        }
+    }
+}
---
+++ Cloud++/Cloud++/Services/FileService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Cloud__.Models;
+using Cloud__.Models.Entities;
+
+namespace Cloud__.Services
+{
+    public class FileService
+    {
+        private ApplicationDbContext _db;
+
+        public FileService()
+        {
+            _db = new ApplicationDbContext();
+        }
+        public void SaveData(string content)
+        {
+            System.Diagnostics.Debug.WriteLine("----DEBUG---");
+            System.Diagnostics.Debug.WriteLine(content);
+            File newFile = new File();
+            newFile.content = content;
+            newFile.fileName = "test";
+            newFile.extension = "css";
+            newFile.folderID = 1;
+
+            _db.Files.Add(newFile);
+            
+            _db.SaveChanges();
+        }
+
+    }
+
+   
+}
